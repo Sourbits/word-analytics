1928c70cb86dd58ac50e1e284f80f9b41e260fa5
Sourbits <a36947325@gmail.com>
2023-12-16 11:01:11 -0800

    Coded check to see how many twitter characters user has left

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const textareaE1 = document.querySelector(".textarea");
+const charactersNumberE1 = document.querySelector(".stat__number--characters");
+const twitterNumberE1 = document.querySelector(".stat__number--twitter");
+const facebookNumberE1 = document.querySelector(".stat__number--facebook");
+const wordsNumberE1 = document.querySelector(".stat__number--words");
 
 textareaE1.addEventListener("input", function () {
   const numberOfCharacters = textareaE1.value.length;
-  const charactersNumberE1 = document.querySelector(
-    ".stat__number--characters"
-  );
+  const twitterCharactersLeft = 280 - numberOfCharacters;
   charactersNumberE1.textContent = numberOfCharacters;
+  twitterNumberE1.textContent = twitterCharactersLeft;
 });
